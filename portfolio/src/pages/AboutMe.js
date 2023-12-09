bc5d410c436cdd956e37c43ab7c5dbf22b785493
Graham <blundell.gp@gmail.com>
2023-12-09 01:18:20 -0800

    added images and text and styled

--- portfolio/src/pages/AboutMe.js
+++ portfolio/src/pages/AboutMe.js
@@ -1,13 +1,55 @@
 import React from 'react'
 import transition from '../pages/transition'
+import js from '../assets/11-javascript.png'
+import html from '../assets/11-html.png'
+import css from '../assets/11-css.png'
+import react from '../assets/11-react.png'
+import ruby from '../assets/11-ruby.png'
+import rails from '../assets/11-rails.png'
+import rspec from '../assets/11-rspec.png'
+import jest from '../assets/11-jest.png'
+import postgres from '../assets/11-postgres.png'
+import thoughtbot from '../assets/11-thoughtbot.png'
+import bootstrap from '../assets/11-bootstrap.png'
+import github from '../assets/11-github.png'
+import git from '../assets/11-git.png'
+import vscode from '../assets/11-vscode.png'
+import linkedin from '../assets/linkedin-logo.png'
+import gitlogo from '../assets/github-logo.png'
+import email from '../assets/email-logo.png'
 import './AboutMe.css'
 
 const AboutMe = () => {
     return (
         <div className='about-div-wrap'>
             <div className='about-div'>
-                <h1>Info about me</h1>
+                <h1>Hi! Thanks for making it this far! I graduated from <a className='learn-link' href='https://learnacademy.org/coding-bootcamp' target='blank'>LEARN Academy</a> in 2023 (Echo Cohort) for Full-Stack Web Development. I also have an associates degree in Science.</h1>
+                <h1>Other than coding, I'm a huge gamer and like to spend my spare time playing games (video or board games).</h1>
+                <h1>My Toolkit</h1>
+            </div>
+            <div className='about-div-tools'>
+                <img className='icon' src={js} />
+                <img className='icon' src={html} />
+                <img className='icon' src={css} />
+                <img className='icon' src={react} />
+                <img className='icon' src={ruby} />
+                <img className='icon' src={rails} />
+                <img className='icon' src={rspec} />
+                <img className='icon' src={jest} />
+                <img className='icon' src={postgres} />
+                <img className='icon' src={thoughtbot} />
+                <img className='icon' src={bootstrap} />
+                <img className='icon' src={github} />
+                <img className='icon' src={git} />
+                <img className='icon' src={vscode} />
+            </div>
+            <div className='h1-contact'>
+                <h1>Contact me</h1>
             </div>    
+            <div className='contact-div'>
+                <img className='icon-1' src={linkedin} />
+                <img className='icon-2' src={gitlogo} />
+            </div>
         </div>
     )
 }
