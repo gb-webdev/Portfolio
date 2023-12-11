1e2f88e0bc916b5381260c9e3495f19ecae59944
Graham <blundell.gp@gmail.com>
2023-12-10 21:15:51 -0800

    resized for mobile devices

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='footer-div'>
-            <h3>Built by Graham Blundell in React</h3>
+            <h3 className='footer-text'>Built by Graham Blundell in React</h3>
         </div>
     )
 }
