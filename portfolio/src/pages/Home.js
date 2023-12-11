e633d6c87bba8647e3c9a5abcc2aacc7315ae295
Graham <blundell.gp@gmail.com>
2023-12-10 21:34:38 -0800

    fixed string error

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <div className='home-div-wrap'>
             <div className='home-div'>
                 <h2>Hi, I'm Graham! Welcome to my Portfolio</h2>
-                <h2>I'm a full stack web web developer in San Diego, CA</h2>
+                <h2>I'm a full stack web developer in San Diego, CA</h2>
                 <NavItem><h2>See my projects <NavLink className='home-link' to='projects'>here</NavLink></h2></NavItem>
                 <NavItem><h2>View/download my resume <NavLink className='home-link' to='resume'>here</NavLink></h2></NavItem>
                 <NavItem><h2>Contact me <NavLink className='home-link' to='about'>here</NavLink></h2></NavItem>
