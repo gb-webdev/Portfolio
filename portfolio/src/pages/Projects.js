16ec7b34b8b5c95462f44133ca4360eb99ec87ed
Graham <blundell.gp@gmail.com>
2023-12-13 19:10:22 -0800

    added email form, updated content and updated firebase

--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -8,7 +8,7 @@ const Projects = () => {
     return (
         <div className='projects-div-wrap'>
             <div className='projects-div'>
-            <h2>Hover over my projects to learn more!</h2>
+            <h2>click on my projects to learn more!</h2>
                 <div className='project-2'>
                     <h2 >Quizard:</h2><a href='https://quizard-u7md.onrender.com/' target='blank'><img className='quizard' src={lizard} alt='A pixel lizard' /></a>
                     <h2>Quizard Front-End source code:</h2><a href='https://github.com/learn-stack-rgb/quizard-frontend' target='blank'><img className='quizard-git-front' src={gitlogo} /></a>
