44d2a130704d1037801e6f2559dbd5d95a2f790e
Graham <blundell.gp@gmail.com>
2024-03-12 23:52:23 -0700

    added new images

--- portfolio/src/pages/AboutMe.js
+++ portfolio/src/pages/AboutMe.js
@@ -1,30 +1,32 @@
 import React from 'react'
 import './AboutMe.css'
-import liz from '../assets/pixel-liz.png'
-import ruby from '../assets/11-ruby.png'
+import codingImg from '../assets/coding.png'
+import debug from '../assets/debug.png'
+import cdeImg from '../assets/coding (1).png'
+import error from '../assets/error.png'
 
 const AboutMe = () => {
     return (
         <section className='about-container' id='about'>
         <h2 className='about-title'>About</h2>
             <div className='about-content'>
-                <img className='about-avatar' src={liz} alt='avatar with a laptop' />
+                <img className='about-avatar' src={codingImg} alt='avatar with a laptop' />
                 <ul className='about-items'>
                     <li className='about-item'>
-                    <img className='about-icon' src={ruby}></img>                        <div className='about-text'>
+                    <img className='about-icon' src={debug}></img>                        <div className='about-text'>
                             <h3>front-end</h3>
                             <p>React, designing, ui/ux</p>
                         </div>
                     </li>  
                     <li className='about-item'>
-                        <img className='about-icon' src={ruby}></img>
+                        <img className='about-icon' src={cdeImg}></img>
                         <div className='about-text'>
                         <h3>back-end</h3>
                         <p>databases, secruity</p>
                         </div>
                     </li>  
                     <li className='about-item'>
-                    <img className='about-icon' src={ruby}></img>                        <div className='about-text'>
+                    <img className='about-icon' src={error}></img>                        <div className='about-text'>
                         <h3>testing</h3>
                         <p>jest and rspec</p>
                         </div>
